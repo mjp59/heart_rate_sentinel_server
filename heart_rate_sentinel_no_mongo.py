1eba48bc7423c8b2d890d4b93a972d17df56ecd4
Michael Postiglione <mjp59@duke.edu>
2018-11-10 18:59:02 -0500

    added docstrings

--- heart_rate_sentinel_no_mongo.py
+++ heart_rate_sentinel_no_mongo.py
@@ -31,11 +31,13 @@ users = [{
 @app.route("/api/status/<patient_id>", methods=["GET"])
 def tachycardic(patient_id):
     """
-   Returns whether the patient is tachycardic
+              Returns if patient is tachycardic
 
-   parameter: patient id- int, patient who's records we are examining
-
-   return: string- string, that contains the output
+    :param patient_id: string
+                Must be the patient id number of an existing patient
+    :return: string
+            tells the user if the patient is tachycardic
+            and when they became tachycardic
     """
     patient_id_int = int(patient_id)
     user = users[patient_id_int]
@@ -58,13 +60,14 @@ def tachycardic(patient_id):
 @app.route("/api/heart_rate/<patient_id>", methods=["GET"])
 def heart_rate(patient_id):
     """
-   Returns all heart rate data from a patient
+                  Returns all heart rate measurements of the patient
 
-   parameter: patient id- int, patient who's records we are examining
-
-   return: list gives a list that contains all heart
-   rate measurments for a patient
-    """
+        :param patient_id: string
+                    Must be the patient id number of an existing patient
+        :return: string
+                returns string that has all heart rate measurements
+                for the patient
+        """
     patient_id_int = int(patient_id)
     user = users[patient_id_int]
     return str(user.get("heart_rate"))
@@ -73,12 +76,13 @@ def heart_rate(patient_id):
 @app.route("/api/heart_rate/average/<patient_id>", methods=["GET"])
 def heart_rate_average(patient_id):
     """
-   Returns all heart rate data from a patient
+              Returns the average heart rate of the patient
 
-   parameter: patient id- int, patient who's records we are examining
-
-   return: list gives a list that contains all heart rate
-   measurments for a patient
+    :param patient_id: string
+                Must be the patient id number of an existing patient
+    :return: string
+            string that contains the
+            average heart rate of the patient
     """
     patient_id_int = int(patient_id)
     user = users[patient_id_int]
@@ -89,127 +93,151 @@ def heart_rate_average(patient_id):
 
 @app.route("/api/new_patient", methods=['POST'])
 def new_patient():
-    # try:
-        r = request.get_json()
-        s1 = r.get("patient_id")
-        s2 = r.get("attending_email")
-        s3 = r.get("user_age")
-        check1 = validate_email(s2)
-        check2 = isinstance(int(s1), int)
-        check3 = isinstance(float(s3), float)
-        u = {
-             "patient_id": s1,
-             "attending_email": s2,
-             "age": s3,
-             "heart_rate": [],
-             "timestamps": [],
-             "tach": False,
-        }
-        if check1 and check2 and check3:
-            users[int(s1)] = u
-            return jsonify(u)
+    """
+                  adds new patient to users list
 
-        return jsonify({"message": "Error occurred, check your inputs"}), 500
+        :return: json
+                returns the information stored for the new
+                patient
+        """
+    # try:
+    r = request.get_json()
+    s1 = r.get("patient_id")
+    s2 = r.get("attending_email")
+    s3 = r.get("user_age")
+    check1 = validate_email(s2)
+    check2 = isinstance(int(s1), int)
+    check3 = isinstance(float(s3), float)
+    u = {
+        "patient_id": s1,
+        "attending_email": s2,
+        "age": s3,
+        "heart_rate": [],
+        "timestamps": [],
+        "tach": False,
+        }
+    if check1 and check2 and check3:
+        users[int(s1)] = u
+        return jsonify(u)
+
+    return jsonify({"message": "Error occurred, check your inputs"}), 500
     # except:
-        # return jsonify({"message": "Error occurred, check your inputs"}), 500
+    # return jsonify({"message": "Error occurred, check your inputs"}), 500
 
 
 @app.route("/api/heart_rate", methods=['POST'])
 def add_heart_rate():
-    # try:
-        r = request.get_json()
-        s1 = r.get("patient_id")
-        s1_int = int(s1)
-        s2 = r.get("heart_rate")
-        s2_float = float(s2)
-        check1 = isinstance(s1_int, int)
-        check2 = isinstance(s2_float, float)
-        if check1 & check2:
-            user = users[s1_int]
-            user_heartrate = user.get("heart_rate")
-            user_heartrate.append(s2_float)
-            user["heart_rate"] = user_heartrate
-            user_timestamps = user.get("timestamps")
-            user_timestamps.append(datetime.now())
-            user["timestamps"] = user_timestamps
-            if s2_float > 159 and (float(user.get("age"))*365) <= 2:
-                user["tach"] = True
-            elif s2_float > 166 and (float(user.get("age"))*365) > 2 \
-                    and (float(user.get("age"))*365) <= 6:
-                user["tach"] = True
-            elif s2_float > 182 and (float(user.get("age"))*365) > 6 \
-                    and (float(user.get("age"))*365) <= 21:
-                user["tach"] = True
-            elif s2_float > 179 and (float(user.get("age"))*365) > 21 \
-                    and (float(user.get("age"))*365) <= 62:
-                user["tach"] = True
-            elif s2_float > 186 and (float(user.get("age"))*365) > 62 \
-                    and (float(user.get("age"))*365) <= 155:
-                user["tach"] = True
-            elif s2_float > 169 and (float(user.get("age"))*365) > 155 \
-                    and (float(user.get("age"))*365) <= 341:
-                user["tach"] = True
-            elif s2_float > 151 and float(user.get("age")) >= 1 \
-                    and float(user.get("age")) <= 2:
-                user["tach"] = True
-            elif s2_float > 137 and float(user.get("age")) > 2 \
-                    and float(user.get("age")) <= 4:
-                user["tach"] = True
-            elif s2_float > 133 and float(user.get("age")) > 4 \
-                    and float(user.get("age")) <= 7:
-                user["tach"] = True
-            elif s2_float > 130 and float(user.get("age")) > 7 \
-                    and float(user.get("age")) <= 11:
-                user["tach"] = True
-            elif s2_float > 119 and float(user.get("age")) > 11 \
-                    and float(user.get("age")) <= 15:
-                user["tach"] = True
-            elif s2_float > 100 and float(user.get("age")) > 15:
-                user["tach"] = True
-            users[s1_int] = user
-            return jsonify(user)
+    """
+                     adds a new heart rate reading to the
+                     patient's info and updates the
+                     tachcardia status flag
 
-        return jsonify({"message": "Error occurred, check your inputs"}), 500
+           :return: json
+                   returns the information stored for the new
+                   patient
+           """
+    # try:
+    r = request.get_json()
+    s1 = r.get("patient_id")
+    s1_int = int(s1)
+    s2 = r.get("heart_rate")
+    s2_float = float(s2)
+    check1 = isinstance(s1_int, int)
+    check2 = isinstance(s2_float, float)
+    if check1 & check2:
+        user = users[s1_int]
+        user_heartrate = user.get("heart_rate")
+        user_heartrate.append(s2_float)
+        user["heart_rate"] = user_heartrate
+        user_timestamps = user.get("timestamps")
+        user_timestamps.append(datetime.now())
+        user["timestamps"] = user_timestamps
+        if s2_float > 159 and (float(user.get("age"))*365) <= 2:
+            user["tach"] = True
+        elif s2_float > 166 and (float(user.get("age"))*365) > 2 \
+                and (float(user.get("age"))*365) <= 6:
+            user["tach"] = True
+        elif s2_float > 182 and (float(user.get("age"))*365) > 6 \
+                and (float(user.get("age"))*365) <= 21:
+            user["tach"] = True
+        elif s2_float > 179 and (float(user.get("age"))*365) > 21 \
+                and (float(user.get("age"))*365) <= 62:
+            user["tach"] = True
+        elif s2_float > 186 and (float(user.get("age"))*365) > 62 \
+                and (float(user.get("age"))*365) <= 155:
+            user["tach"] = True
+        elif s2_float > 169 and (float(user.get("age"))*365) > 155 \
+                and (float(user.get("age"))*365) <= 341:
+            user["tach"] = True
+        elif s2_float > 151 and float(user.get("age")) >= 1 \
+                and float(user.get("age")) <= 2:
+            user["tach"] = True
+        elif s2_float > 137 and float(user.get("age")) > 2 \
+                and float(user.get("age")) <= 4:
+            user["tach"] = True
+        elif s2_float > 133 and float(user.get("age")) > 4 \
+                and float(user.get("age")) <= 7:
+            user["tach"] = True
+        elif s2_float > 130 and float(user.get("age")) > 7 \
+                and float(user.get("age")) <= 11:
+            user["tach"] = True
+        elif s2_float > 119 and float(user.get("age")) > 11 \
+                and float(user.get("age")) <= 15:
+            user["tach"] = True
+        elif s2_float > 100 and float(user.get("age")) > 15:
+            user["tach"] = True
+        users[s1_int] = user
+        return jsonify(user)
+
+    return jsonify({"message": "Error occurred, check your inputs"}), 500
     # except:
-        # return jsonify({"message": "Error occurred, check your inputs"}), 500
+    # return jsonify({"message": "Error occurred, check your inputs"}), 500
 
 
 @app.route("/api/heart_rate/interval_average", methods=['POST'])
 def interval_average():
-    # try:
-        r = request.get_json()
-        s1 = r.get("patient_id")
-        s1_int = int(s1)
-        s2 = r.get("heart_rate_average_since")
-        date_time_obj = datetime.strptime(str(s2), "%Y-%m-%d %H:%M:%S.%f")
-        check1 = isinstance(s1_int, int)
-        if check1:
-            user = users[s1_int]
-            times = user.get("timestamps")
-            index = 0
-            for x in range(len(times)):
-                if times[x].date() > date_time_obj.date():
-                    index = x
-                    break
-                if times[x].date() == date_time_obj.date() \
-                        and times[x].time() > date_time_obj.time():
-                    index = x
-                    break
-                index = x
-            heartrates = user.get("heart_rate")
-            if index == 0:
-                average = 0
-            else:
-                sum = 0
-                for i in range(index+1):
-                    sum = sum + heartrates[i]
-                average = sum/(index+1)
-            return jsonify({"average": average,
-                            "Time:": date_time_obj})
+    """
+                     returns the average heart rate of a
+                     patient up to a certain date
 
-        return jsonify({"message": "Error occurred, check your inputs"}), 500
+           :return: json
+                   returns the information stored for the new
+                   patient
+           """
+    # try:
+    r = request.get_json()
+    s1 = r.get("patient_id")
+    s1_int = int(s1)
+    s2 = r.get("heart_rate_average_since")
+    date_time_obj = datetime.strptime(str(s2), "%Y-%m-%d %H:%M:%S.%f")
+    check1 = isinstance(s1_int, int)
+    if check1:
+        user = users[s1_int]
+        times = user.get("timestamps")
+        index = 0
+        for x in range(len(times)):
+            if times[x].date() > date_time_obj.date():
+                index = x
+                break
+            if times[x].date() == date_time_obj.date() \
+                    and times[x].time() > date_time_obj.time():
+                index = x
+                break
+            index = x
+        heartrates = user.get("heart_rate")
+        if index == 0:
+            average = 0
+        else:
+            sum = 0
+            for i in range(index+1):
+                sum = sum + heartrates[i]
+                average = sum/(index+1)
+        return jsonify({"average": average,
+                        "Time:": date_time_obj})
+
+    return jsonify({"message": "Error occurred, check your inputs"}), 500
     # except:
-        # return jsonify({"message": "Error occurred, check your inputs"}), 500
+    # return jsonify({"message": "Error occurred, check your inputs"}), 500
 
 
 if __name__ == "__main__":
